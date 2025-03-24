7a0b47195ff33515c16b76185fd38b101288da7e
Hiral Kanakhara <kanakharahiral112@gmail.com>
2025-03-24 19:51:21 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,30 @@
+a=5
+b=2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) #Answer always comes in float
+print(a%b) #remainder
+print(a**b) #a^b
+
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a>b)
+print(a<=b)
+print(a<b)
+
+num=10
+num+=10
+print("Num:",num)
+
+print(not False)
+print(not True)
+print(not (a>b))
+
+val1=True
+val2=False
+print("and operator:", val1 and val2)
+print("or operator:", val1 or val2)
+
