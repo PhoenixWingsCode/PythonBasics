4724c2be81eab0f95b9b4b9caea6e9f406af98ff
Hiral Kanakhara <kanakharahiral112@gmail.com>
2025-03-25 19:58:46 +0530

    Conditions

---
+++ Conditions.py
@@ -0,0 +1,20 @@
+age=24
+
+if(age>=18):
+    print("can vote")
+
+light="green"
+
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("look")
+elif(light=="green"):
+    print("go")
+else:
+    print("light is broken")
+
+if(age>=18):
+    print("can vote")#indentation=tab spaces not using {} braces
+else:
+    print("cannot vote")
